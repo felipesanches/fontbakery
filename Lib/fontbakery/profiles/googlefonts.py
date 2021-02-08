b6f1bf8334ced7b7a2ce2b87bc09de2861b9d0c7
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2021-02-08 15:54:32 -0300

    WIP: draft of OT Feature checks proposed by Denis Moyogo

--- Lib/fontbakery/profiles/googlefonts.py
+++ Lib/fontbakery/profiles/googlefonts.py
@@ -156,7 +156,9 @@ FONT_FILE_CHECKS = [
     'com.google.fonts/check/gf-axisregistry/fvar_axis_defaults',
     'com.google.fonts/check/STAT/gf-axisregistry',
     'com.google.fonts/check/STAT/axis_order',
-    'com.google.fonts/check/mandatory_avar_table'
+    'com.google.fonts/check/mandatory_avar_table',
+    'com.google.fonts/check/stylisticset_description',
+    'com.google.fonts/check/missing_small_caps_glyphs'
 ]
 
 GOOGLEFONTS_PROFILE_CHECKS = \
@@ -5109,6 +5111,75 @@ def com_google_fonts_check_mandatory_avar_table(ttFont):
     else:
         yield PASS, "OK"
 
+
+@check(
+    id = 'com.google.fonts/check/missing_small_caps_glyphs',
+    rationale = """
+        Ensure small caps glyphs are available if a font declares smcp or c2sc OT features
+    """,
+    misc_metadata = {
+        'request': 'https://github.com/googlefonts/fontbakery/issues/3154'
+    }
+)
+def com_google_fonts_check_missing_small_caps_glyphs(ttFont):
+    """..."""
+
+    passed = True
+    if 'GSUB' in ttFont and ttFont['GSUB'].table.FeatureList is not None:
+        llist = ttFont['GSUB'].table.LookupList
+        for record in range(ttFont['GSUB'].table.FeatureList.FeatureCount):
+            feature = ttFont['GSUB'].table.FeatureList.FeatureRecord[record]
+            tag = feature.FeatureTag
+            if tag == 'smcp':
+                for index in feature.Feature.LookupListIndex:
+                    smcp_glyphs = set(llist.Lookup[index].SubTable[0].mapping.values())
+                    missing = smcp_glyphs - set(ttFont.getGlyphNames())
+                    if missing:
+                        passed = False
+                        yield FAIL,\
+                              Message('missing-scmp-glyphs',
+                                      f"{missing}")
+                break
+
+    if passed:
+        yield PASS, "OK"
+
+
+@check(
+    id = 'com.google.fonts/check/stylisticset_description',
+    rationale = """
+        Stylistic sets must provide description text. Programs such as InDesign, TextEdit, Inkscape use that info to display to the users so that they know what a given stylistic set offers.
+    """,
+    misc_metadata = {
+        'request': 'https://github.com/googlefonts/fontbakery/issues/3155'
+    }
+)
+def com_google_fonts_check_stylisticset_description(ttFont):
+    """..."""
+
+    passed = True
+    if 'GSUB' in ttFont and ttFont['GSUB'].table.FeatureList is not None:
+        llist = ttFont['GSUB'].table.LookupList
+        for record in range(ttFont['GSUB'].table.FeatureList.FeatureCount):
+            feature = ttFont['GSUB'].table.FeatureList.FeatureRecord[record]
+            tag = feature.FeatureTag
+            SSETS = [f'ss{n+1:02d}' for n in range(20)]
+            assert('ss00' not in SSETS)
+            assert('ss01' in SSETS)
+            assert('ss20' in SSETS)
+            assert('ss21' not in SSETS)
+            if tag in SSETS:
+                if feature.Feature.FeatureParams == None:
+                    passed = False
+                    yield FAIL,\
+                          Message('missing-description',
+                                  f"The stylistic set {tag} lacks a description string on the 'name' table.")
+                else:
+                    # TODO: maybe here we can add code to make sure that the referenced nameid does exist in the name table.
+                break
+
+    if passed:
+        yield PASS, "OK"
 
 ###############################################################################
 
